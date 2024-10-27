1771b86f2b6c47ff592d0e93952fec95c1768699
Tamilarasi Kannan <115526719+tamilarasi-kannan@users.noreply.github.com>
2024-10-27 12:52:07 +0530

    Create common.sv

---
+++ Testbench/common.sv
@@ -0,0 +1,7 @@
+mailbox gen2bfm=new();
+mailbox mon2cov=new();
+mailbox mon2sbd=new();
+parameter n=3;
+static int num_matches;  // If a varaiable gets static then it can be accessed everywhere.
+static int num_miss_matches;
+static int  count=3;
